a289543304ebb89c20fef9a91ecbaf2b4fc3baca
our1314 <our1314@foxmail.com>
2022-12-27 14:49:34 +0800

    xx

--- 图像搜索/Program.cs
+++ 图像搜索/Program.cs
@@ -10,34 +10,50 @@ var searchpath = Console.ReadLine();
 
 
 //1、被搜索图像
-var temp_src = new Mat(searchimage, ImreadModes.Unchanged);
-var temp = temp_src.Clone();
+var temp = new Mat(searchimage, ImreadModes.Color);
 
 //2、搜索
 var images = new DirectoryInfo(searchpath).GetFiles("*", searchOption: SearchOption.AllDirectories);
+images = images.Where(p => p.Extension == ".bmp" || p.Extension == ".png" || p.Extension == ".jpg").ToArray();
 images = images.OrderBy((p) =>
 {
-    var img = new Mat(p.FullName, ImreadModes.Unchanged);
+    var img = new Mat(p.FullName, ImreadModes.Color);
     Mat diff = new Mat();
+    Cv2.Absdiff(temp, img, diff);
 
-    img = img.Resize(temp.Size());
-    img.ConvertTo(img, MatType.CV_8S);
-    temp.ConvertTo(temp, MatType.CV_8S);
+    var sum = diff.Sum().Val0;
+    return sum;
 
-    Cv2.Subtract(temp, img, diff);
-    Mat abs = diff.Abs();
-    var sum = abs.Sum();
-    return sum.Val0;
+    //img = img.Resize(temp.Size());
+    //img.ConvertTo(img, MatType.CV_8S);
+    //temp.ConvertTo(temp, MatType.CV_8S);
+
+    //Cv2.Subtract(temp, img, diff);
+    //Mat abs = diff.Abs();
+    //var sum = abs.Sum();
+    //return sum.Val0;
 }).ToArray();
 
 //3、最相似的图像
-var f1 = new Mat(images.First().FullName, ImreadModes.Unchanged);
-f1 = f1.Resize(temp_src.Size());
+var f1 = new Mat(images.First().FullName, ImreadModes.Color);
+f1 = f1.Resize(temp.Size());
 
 //4、显示
 var dis = new Mat();
-Cv2.HConcat(temp_src, f1, dis);
+var I = new Mat(temp.Rows, 10, temp.Type(), Scalar.Black);
+Cv2.HConcat(new[] { temp, I, f1 }, dis);
+
+//如果目标尺寸过大则按比例进行缩放
+var w = 1500.0;
+var max = Math.Max(dis.Width, dis.Height);
+if (max > w)
+{
+    var f = w / max;
+    dis = dis.Resize(new Size(), f, f);
+}
+
+
 dis = dis.CopyMakeBorder(0, 40, 0, 0, BorderTypes.Constant);
-dis.PutText(images[0].FullName, new Point(0, dis.Height - 10), HersheyFonts.HersheyComplex, 0.3, Scalar.Red, 1); ;
+dis.PutText(images[0].FullName, new Point(0, dis.Height - 10), HersheyFonts.HersheyComplex, 0.3, Scalar.Red, 1);
 Cv2.ImShow(images[0].Name, dis);
 Cv2.WaitKey();
