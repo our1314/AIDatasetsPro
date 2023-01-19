074a6c4d6585488a4a93e4ff9ab1e472e0c98aad
our1314 <our1314@foxmail.com>
2023-01-19 15:47:27 +0800

    xx

--- 将目录下的图像全部改为指定格式/Program.cs
+++ 将目录下的图像全部改为指定格式/Program.cs
@@ -3,7 +3,7 @@
 var img_ext = new[] { ".png", ".jpg", ".bmp", ".jif" };
 var target_ext = ".jpg";
 
-Console.WriteLine("输入文件夹路径：(可以拖拽!)");
+Console.WriteLine($"将路径下的图像全部改为{target_ext}格式，输入文件夹路径：(可以拖拽!)");
 var path = Console.ReadLine();
 var files = new DirectoryInfo(path).GetFiles();
 files = files.Where(f => img_ext.Contains(f.Extension) && f.Extension != target_ext).ToArray();
