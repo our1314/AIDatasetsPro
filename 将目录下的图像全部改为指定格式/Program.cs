df8a6559d048f431f4176778b096046c5a47874c
our1314 <our1314@foxmail.com>
2023-01-19 15:44:48 +0800

    xx

---
+++ 将目录下的图像全部改为指定格式/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenCvSharp;
+
+var img_ext = new[] { ".png", ".jpg", ".bmp", ".jif" };
+var target_ext = ".jpg";
+
+Console.WriteLine("输入文件夹路径：(可以拖拽!)");
+var path = Console.ReadLine();
+var files = new DirectoryInfo(path).GetFiles();
+files = files.Where(f => img_ext.Contains(f.Extension) && f.Extension != target_ext).ToArray();
+
+foreach (var f in files)
+{
+    var img = new Mat(f.FullName, ImreadModes.Unchanged);
+    img.SaveImage(f.FullName.Replace(f.Extension, target_ext));
+}
