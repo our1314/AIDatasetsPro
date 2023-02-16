5de500718be9c1bb40b5fe8ec127a9fc15fccfa3
our1314 <our1314@foxmail.com>
2023-02-16 09:33:16 +0800

    xx

--- AIDatasetsPro/src/test_生成目标检测和语义分割数据集.cs
+++ AIDatasetsPro/src/test_生成目标检测和语义分割数据集.cs
@@ -8,6 +8,19 @@ namespace AIDatasetsPro.src
     {
         public override void RunTest()
         {
+            #region 参数设置
+            // 生成图像的总数
+            var cnt_sum = 100;
+
+            // 每张背景图的贴图数量
+            var cnt_perimg = 1;
+
+            var colors = new[] { new Scalar(0, 0, 128), new Scalar(0, 128, 0), new Scalar(128, 0, 0),
+                                     new Scalar(0, 128, 128), new Scalar(128, 0, 128), new Scalar(128, 128, 0),
+                                     new Scalar(128, 128, 128)};
+            #endregion
+
+
             Console.WriteLine("给定目标文件夹路径，其中包含前景图(forexxx.png，四通道图像（透明）)和背景图(格式任意)，可以多个");
             Console.WriteLine("输入包含前景图和背景图的文件夹路径：");
 
@@ -26,27 +39,22 @@ namespace AIDatasetsPro.src
             var files_fore = files.Where(f => f.Name.Contains("fore")).ToArray();
             var files_back = files.Where(f => f.Name.Contains("back")).ToArray();
 
-            // 生成图像的总数
-            var cnt_sum = 100;
-
-            // 每张背景图的贴图数量
-            var cnt_perimg = 1;
-
-
             for (int i = 0; i < cnt_sum; i++)
             {
-                var result_labels = "";
-                // 0、随机获取背景图
+                var gen_seg_image = new Mat();
+                var gen_seg_mask = new Mat();
+                var gen_yolo_labels = "";
+
+                // 0、随机获取一张背景图
                 var index_back = new Random().Next(files_fore.Length);
-                var back = new Mat(files_back[index_back].FullName, ImreadModes.Color);
-                var back_mask = back.EmptyClone().CvtColor(ColorConversionCodes.BGR2GRAY);
-                var color_mask = back.EmptyClone();
+                var back = new Mat(files_back[index_back].FullName, ImreadModes.Color);//背景图像
+                var black = back.EmptyClone().SetTo(0);//与背景图同样尺寸的黑色图像
 
                 for (int j = 0; j < cnt_perimg; j++)
                 {
-                    //1、随机获取前景图
-                    var index_fore = new Random().Next(files_fore.Length);
-                    var fore = new Mat(files_fore[index_fore].FullName, ImreadModes.Unchanged);
+                    //1、随机获取一张前景图
+                    var random_index = new Random().Next(files_fore.Length);
+                    var fore = new Mat(files_fore[random_index].FullName, ImreadModes.Unchanged);
 
                     //2、将四通道的前景图拆分出bgr图像和mask图像
                     var fore_chls = fore.Split();
@@ -54,17 +62,16 @@ namespace AIDatasetsPro.src
                     Cv2.Merge(fore_chls.Take(3).ToArray(), bgr);
                     var mask = fore_chls.Last();
 
-                    //3、生成随机坐标，并贴图
+                    //3、生成随机坐标
                     var row = new Random().Next(0, back.Rows - fore.Rows);//不会生成最大值
                     var col = new Random().Next(0, back.Cols - fore.Cols);
-
                     var rect = new Rect(col, row, fore.Cols, fore.Rows);
 
+                    //4、生成目标图像和同尺寸的mask图像
                     bgr.CopyTo(back[rect], mask);//将前景图贴在背景图上
-                    mask.CopyTo(back_mask[rect], mask);//将mask贴在同样尺寸的黑色图像上                    
-                    color_mask.SetTo(new Scalar(0, 0, 128), back_mask);
+                    black[rect].SetTo(colors[j], mask);//
 
-                    //4、计算label
+                    //5、计算yolo标签
                     double x1 = col;
                     double y1 = row;
                     double w = fore.Cols;
@@ -74,7 +81,7 @@ namespace AIDatasetsPro.src
                     var cy = y1 + h / 2.0;//贴图中心坐标
 
                     var label_yolo = $"0 {(cx / back.Width).ToString("F12")} {(cy / back.Height).ToString("F12")} {(w / back.Width).ToString("F12")} {(h / back.Height).ToString("F12")}";
-                    result_labels += label_yolo + "\r\n";
+                    gen_yolo_labels += label_yolo + "\r\n";
                 }
 
                 //5、保存
@@ -88,19 +95,18 @@ namespace AIDatasetsPro.src
                     var padleft = (maxlen - back.Width) / 2;
                     back = back.CopyMakeBorder(padup, padup, padleft, padleft, BorderTypes.Constant);
 
-                    color_mask = color_mask.Resize(new Size(), f, f, InterpolationFlags.Nearest);
-                    maxlen = Math.Max(color_mask.Width, color_mask.Height);
-                    padup = (maxlen - color_mask.Height) / 2;
-                    padleft = (maxlen - color_mask.Width) / 2;
-                    color_mask = color_mask.CopyMakeBorder(padup, padup, padleft, padleft, BorderTypes.Constant);
+                    black = black.Resize(new Size(), f, f, InterpolationFlags.Nearest);
+                    maxlen = Math.Max(black.Width, black.Height);
+                    padup = (maxlen - black.Height) / 2;
+                    padleft = (maxlen - black.Width) / 2;
+                    black = black.CopyMakeBorder(padup, padup, padleft, padleft, BorderTypes.Constant);
                 }
                 #endregion
 
                 var name = work.Work.Now;
                 back.ImSave(@$"{path_images}\{name}.jpg");
-                result_labels.Trim().StrSave(@$"{path_labels}\{name}.txt");
-                //back_mask.ImSave(@$"{path_masks}\{name}.png");
-                color_mask.ImSave(@$"{path_masks}\{name}.png");
+                gen_yolo_labels.Trim().StrSave(@$"{path_labels}\{name}.txt");
+                black.ImSave(@$"{path_masks}\{name}.png");
 
                 //6、显示
                 CV.ImShow("dis", back);
