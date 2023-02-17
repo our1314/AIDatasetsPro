47a2b65de400a7c6f2b039070c8e67df377b4ebd
liulin <liulin@apscd.com>
2023-02-18 07:49:03 +0800

    xx

--- AIDatasetsPro/src/test_xxxxxxxxxxxxxxxxxxxxxxxxx.cs
+++ AIDatasetsPro/src/test_xxxxxxxxxxxxxxxxxxxxxxxxx.cs
@@ -1,13 +1,33 @@
 ﻿using OpenCvSharp;
 using work.cv;
 using work.test;
-
+using static System.Math;
 namespace AIDatasetsPro.src
 {
     internal class test_xxxxxxxxxxxxxxxxxxxxxxxxx : ConsoleTestBase
     {
         public override void RunTest()
         {
+            var p = new Mat(3, 3, MatType.CV_64FC1, new double[,]
+            {
+                {Cos(PI/4),0,-Cos(PI/4)},
+                {Sin(PI/4) ,0,Sin(PI/4)},
+                {0 ,-1,0}
+            });
+            //var p = new Mat(3, 3, MatType.CV_64FC1, new double[,]
+            //{
+            //    {-Cos(PI/4),0,-Cos(PI/4)},
+            //    {-Sin(PI/4),0, Sin(PI/4)},
+            //    {0 ,-1,0}
+            //});
+            //Console.WriteLine((p.Col(0).T() * p.Col(1)).ToMat().Dump());
+            //Console.WriteLine((p.Col(1).T() * p.Col(2)).ToMat().Dump());
+            //Console.WriteLine((p.Col(2).T() * p.Col(0)).ToMat().Dump());
+
+            var a = work.math.MathExp._SO3(p);
+            Console.WriteLine(a.Dump());
+            return;
+
             Console.WriteLine("输入图像文件：");
             var path = Console.ReadLine();//.Replace("\"", "");
             var src = new Mat(path, ImreadModes.Unchanged);
