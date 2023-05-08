c748f3ab328fcb415c122710cdb1e97b85033444
our1314 <our1314@foxmail.com>
2023-05-08 12:31:38 +0800

    xx

--- AIDatasetsPro/src/test_xray毛刺自动标注.cs
+++ AIDatasetsPro/src/test_xray毛刺自动标注.cs
@@ -113,7 +113,7 @@ namespace AIDatasetsPro.src
 
                 HOperatorSet.GenRectangle1(out HObject ModelRegion, region_coord[0], region_coord[1], region_coord[2], region_coord[3]);
                 //var dis = CreateScaledShapeModel(img_temp, ModelRegion, contrast, mincontrast, scaleMin: 0.5, scaleMax: 2.0);
-                //var dis = CreateNccModel(img_temp, ModelRegion, 0, 0);
+                var dis = CreateNccModel(img_temp, ModelRegion, 0, 0);
                 Cv2.ImShow("dis", dis);
                 Cv2.WaitKey();
                 Cv2.DestroyAllWindows();
